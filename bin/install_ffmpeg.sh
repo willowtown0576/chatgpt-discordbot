581d381900e6b343f223df40e8272cba2d16b65e
Keisuke Yanagimachi <willowtown0576@gmail.com>
2024-05-25 18:29:05 +0900

    Fix install_ffmpeg.sh

--- bin/install_ffmpeg.sh
+++ bin/install_ffmpeg.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-# Install necessary packages
+# Update package list
 apt-get update
-apt-get install -y ffmpeg libasound2 libasound2-dev libpulse0 libpulse-dev libpulsedsp libpulse-mainloop-glib0
+
+# Add deb-multimedia repository for ffmpeg
+echo "deb http://www.deb-multimedia.org stretch main non-free" >> /etc/apt/sources.list
+apt-get update
+
+# Install deb-multimedia-keyring to avoid key verification errors
+apt-get install -y --allow-unauthenticated deb-multimedia-keyring
+apt-get update
+
+# Install necessary packages
+apt-get install -y --allow-unauthenticated ffmpeg libasound2 libasound2-dev libpulse0 libpulse-dev libpulsedsp libpulse-mainloop-glib0
 
 # Clean up
 apt-get clean
